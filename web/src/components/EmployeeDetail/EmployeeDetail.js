d29951f835f15abf3810d93983f5d99add92e538
khanhnhanbk <nhanhoa21012002@gmail.com>
2022-11-23 14:45:03 +0700

    add detail employee

--- web/src/components/EmployeeDetail/EmployeeDetail.js
+++ web/src/components/EmployeeDetail/EmployeeDetail.js
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Calendar from 'react-calendar'
+import UpcomingShift from '../UpcomingShift/UpcomingShift';
+import 'react-calendar/dist/Calendar.css';
 
 const EmployeeDetail = (probs) => {
+  const [value, onChange] = useState(new Date());
+
   return (
     <div>
-        <h1>{probs.name}</h1>
-        <p>{probs.email}</p>
-
-        <div>Calender</div>
+      <div className='employeeInfo'>
+        <h3>{probs.name}</h3>
+        <p>Email: {probs.email}</p>
+        <p>Role: {probs.role}</p>
+        <p>Exp: {probs.exp}</p>
+        <p>Date of birth: {probs.dateOfBirth}</p>
+        <p>WorkSpace: {probs.workspace}</p>
+      </div>
+      <div>
+        <Calendar onChange={onChange} value={value} />
+      </div>
+      <div>
+        <h3>Upcoming shift</h3>
+        <UpcomingShift />
+      </div>
     </div>
   )
 }
