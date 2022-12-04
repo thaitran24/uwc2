265eefe8171651b9dd29621417d4ef25c531be4b
ptmsk <th.n.h.2.40.31@gmail.com>
2022-12-04 15:56:57 +0700

    add google api to route screen

--- mobile/src/screens/Routes/Routes.js
+++ mobile/src/screens/Routes/Routes.js
@@ -1,12 +1,51 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Dimensions, StyleSheet, Text, View } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import React from "react";
+
+const { width, height } = Dimensions.get("window");
+const ASPECT_RATIO = width / height;
+const LATITUDE_DELTA = 0.02;
+const LONGTITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+const INITIAL_POSITION = {
+  latitude: 40.76711,
+  longtitude: -73.979704,
+  // latitude: 10.762622,
+  // longtitude: 106.660172,
+  latitudeDelta: LATITUDE_DELTA,
+  longtitudeDelta: LONGTITUDE_DELTA,
+};
 
 export default function Routes() {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Routes</Text>
+      <MapView
+        style={styles.map}
+        provider={PROVIDER_GOOGLE}
+        initialRegion={INITIAL_POSITION}
+      />
+      {/* <GooglePlacesAutocomplete
+        placeholder="Search"
+        onPress={(data, details = null) => {
+          // 'details' is provided when fetchDetails = true
+          console.log(data, details);
+        }}
+        query={{
+          key: "YOUR API KEY",
+          language: "en",
+        }}
+      /> */}
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: "100%",
+    height: "80%",
+  },
+});
